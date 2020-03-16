3e31f3c29338a18abd4ba8d8fe462ff7d0cf8865
joanff <joanff@qq.com>
2020-03-16 09:36:55 -0400

    add comment

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,17 +13,20 @@ print(res)
 
 # Creating rooms
 # add a room named: testroom2
+# return new room_id
 res = post('http://localhost:5000/create_room', json={'room':{'name':'testroom5'}}).json()
 print(res)
 #
 #
 # Adding messages to rooms
 # add a message content='test content two', room_id=2
+# return new message_id
 res = post('http://localhost:5000/add_msg', json={'message':{'content':'test content kaka', 'room_id':3}}).json()
 print(res)
 
 # "liking" a message
 # add a like user_id=1, room_id=1
+# return new like_id
 res = post('http://localhost:5000/add_like', json={'user_id':2, 'message_id':3}).json()
 print(res)
 

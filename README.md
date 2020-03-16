279fe5071ae06eceeebb0386091010f68317f712
joanff <joanff@qq.com>
2020-03-16 09:35:31 -0400

    fix readme

--- README.md
+++ README.md
@@ -4,10 +4,26 @@
 ```
 docker-compose build
 docker-compose up
+
+after it's done
+run
+docker-compose down
 ```
 
 #### how to test
 python test/test_api.py
-or use browser
 
-http://localhost:5000/get_rooms
+or use browser: http://localhost:5000/get_rooms
+
+if you fail unfortunately :flushed:
+
+try to do the following again
+```
+docker-compose down
+docker-compose up
+```
+
+
+
+
+
--- tests/test_api.py
+++ tests/test_api.py
@@ -13,17 +13,17 @@ print(res)
 
 # Creating rooms
 # add a room named: testroom2
-res = post('http://localhost:5000/create_room', json={'room':{'name':'testroom2'}}).json()
+res = post('http://localhost:5000/create_room', json={'room':{'name':'testroom5'}}).json()
 print(res)
-
-
+#
+#
 # Adding messages to rooms
 # add a message content='test content two', room_id=2
-res = post('http://localhost:5000/add_msg', json={'message':{'content':'test content two', 'room_id':2}}).json()
+res = post('http://localhost:5000/add_msg', json={'message':{'content':'test content kaka', 'room_id':3}}).json()
 print(res)
 
 # "liking" a message
 # add a like user_id=1, room_id=1
-res = post('http://localhost:5000/add_like', json={'user_id':1, 'message_id':1}).json()
+res = post('http://localhost:5000/add_like', json={'user_id':2, 'message_id':3}).json()
 print(res)
 
